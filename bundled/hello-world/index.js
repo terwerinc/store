a7d8dfcb1a98918e0e21a16a126e1aaa9168bf7a
terwer <youweics@163.com>
2023-12-19 15:09:02 +0800

    feat: without bundle, use iife

--- bundled/hello-world/index.js
+++ bundled/hello-world/index.js
@@ -1,29 +1,33 @@
-export default class HelloWorldAdaptor extends globalThis.ptapi.BaseBlogApi {
-    async postConstruct() {
-        console.log("HelloWorldAdaptor mounted")
+(() => {
+    class HelloWorldAdaptor extends globalThis.ptapi.BaseBlogApi {
+        async postConstruct() {
+            console.log("HelloWorldAdaptor mounted")
+        }
+
+        async beforeDestroy() {
+            console.log("HelloWorldAdaptor unmounted")
+        }
+
+        async getMetaData() {
+            console.log("getMetaData invoked at HelloWorldAdaptor")
+        }
+
+        async getUsersBlogs() {
+            console.log("getUsersBlogs invoked at HelloWorldAdaptor")
+        }
+
+        async newPost(post, publish = true) {
+            console.log("newPost invoked at HelloWorldAdaptor")
+        }
+
+        async editPost(postid, post, publish = true) {
+            console.log("editPost invoked at HelloWorldAdaptor")
+        }
+
+        async deletePost(postid) {
+            console.log("deletePost invoked at HelloWorldAdaptor")
+        }
     }
 
-    async beforeDestroy() {
-        console.log("HelloWorldAdaptor unmounted")
-    }
-
-    async getMetaData() {
-        console.log("getMetaData invoked at HelloWorldAdaptor")
-    }
-
-    async getUsersBlogs() {
-        console.log("getUsersBlogs invoked at HelloWorldAdaptor")
-    }
-
-    async newPost(post, publish = true) {
-        console.log("newPost invoked at HelloWorldAdaptor")
-    }
-
-    async editPost(postid, post, publish = true) {
-        console.log("editPost invoked at HelloWorldAdaptor")
-    }
-
-    async deletePost(postid) {
-        console.log("deletePost invoked at HelloWorldAdaptor")
-    }
-}
+    globalThis.ptapi.registerAdaptor(HelloWorldAdaptor)
+})()

b0dd60f70eff09b01a8a8a08b00b2efab4cdfa3d
terwer <youweics@163.com>
2024-01-01 18:42:13 +0800

    feat: update metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,6 @@
-import React from "react";
-import Head from "next/head";
-
 export default function Home() {
-    return <>
-        <Head>
-            <title>Publisher Store</title>
-            <meta name="description" content="This is a series of publishing tool plugins officially released by Terwer Inc." />
-            <meta name="keywords" content="Store, Publisher, Plugin, Platform" />
-        </Head>
+    return <div>
         <h1>Welcome to Publisher Store!</h1>
         <p>© 2010-2024 Terwer Inc. All Rights Reserved.</p>
-    </>;
+    </div>;
 }

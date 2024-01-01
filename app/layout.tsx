85aab93c4314e8fe4b03bfd9857afdf56dc20095
terwer <youweics@163.com>
2024-01-01 18:42:44 +0800

    feat: update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 export const metadata = {
-  title: 'Publisher Store',
-  description: 'A series of publishing tool plugins officially released by Terwer Inc.',
-  keywords: 'Store, Publisher, Plugin, Platform'
+  title: "Publisher Store",
+  description: "A series of publishing tool plugins officially released by Terwer Inc.",
+  keywords: "Store, Publisher, Plugin, Platform"
 }
 
 export default function RootLayout({
